9119208518888f79f52ab2a9503c7ccb728123dd
nyasia <nyasiav@mail.usf.edu>
2019-06-16 00:51:42 -0400

    Canvas API implementation

--- Connect-A-Bull/Calendar.Designer.cs
+++ Connect-A-Bull/Calendar.Designer.cs
@@ -28,44 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.calandar_browser = new System.Windows.Forms.WebBrowser();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // calandar_browser
+            // label1
             // 
-            this.calandar_browser.Location = new System.Drawing.Point(49, 84);
-            this.calandar_browser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.calandar_browser.Name = "calandar_browser";
-            this.calandar_browser.Size = new System.Drawing.Size(673, 419);
-            this.calandar_browser.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(449, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Classes";
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.Location = new System.Drawing.Point(49, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(307, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(362, 56);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
             // 
             // Calendar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.calandar_browser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Calendar";
-            this.Size = new System.Drawing.Size(805, 640);
+            this.Size = new System.Drawing.Size(1073, 788);
+            this.Load += new System.EventHandler(this.Calendar_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -73,8 +67,7 @@
 
         #endregion
 
-        private System.Windows.Forms.WebBrowser calandar_browser;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Connect-A-Bull/Calendar.cs
+++ Connect-A-Bull/Calendar.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Connect_A_Bull
 {
@@ -15,6 +18,24 @@ namespace Connect_A_Bull
         public Calendar()
         {
             InitializeComponent();
+        }
+
+        private void Calendar_Load(object sender, EventArgs e)
+        {
+            var json_data = "";
+            using (WebClient wc = new WebClient())
+            {
+                 json_data= wc.DownloadString("https://canvas.instructure.com/api/v1/users/self/courses?enrollment_state=active&access_token=13~UR54ZnsYuFhKMGipOJ9kn8yZX9gpd5IYOAp0zpYVYmZRC52GdZupc1qj4q59j8Qa");
+            }
+
+            JArray jsonArray = JArray.Parse(json_data);
+            var jsonObjects = jsonArray.OfType<JObject>().ToList();
+
+            label2.Text = jsonObjects[0].ToString();
+
+
+
+
         }
     }
 }
