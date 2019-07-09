2c94e3437c57bb6f79cd883c8c79b9b76e8c3778
Brittany <xbritbx@gmail.com>
2019-07-09 14:01:09 -0400

    Trying to add Calendar

--- Connect-A-Bull/Calendar.Designer.cs
+++ Connect-A-Bull/Calendar.Designer.cs
@@ -28,57 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.coursesCheckListBox = new System.Windows.Forms.CheckedListBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
-            // label1
+            // dateTimePicker
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(337, 84);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Classes";
+            this.dateTimePicker.Location = new System.Drawing.Point(4, 4);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker.TabIndex = 2;
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.DateTimePicker1_ValueChanged);
             // 
-            // label2
+            // coursesCheckListBox
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(189, 132);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.coursesCheckListBox.FormattingEnabled = true;
+            this.coursesCheckListBox.Location = new System.Drawing.Point(731, 525);
+            this.coursesCheckListBox.Name = "coursesCheckListBox";
+            this.coursesCheckListBox.Size = new System.Drawing.Size(265, 191);
+            this.coursesCheckListBox.TabIndex = 5;
             // 
-            // dateTimePicker1
+            // treeView1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(264, 171);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 2;
+            this.treeView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(147)))), ((int)(((byte)(116)))));
+            this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.Location = new System.Drawing.Point(4, 33);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.ShowLines = false;
+            this.treeView1.ShowPlusMinus = false;
+            this.treeView1.Size = new System.Drawing.Size(1066, 752);
+            this.treeView1.TabIndex = 6;
             // 
             // Calendar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.coursesCheckListBox);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.dateTimePicker);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Calendar";
-            this.Size = new System.Drawing.Size(805, 640);
+            this.Size = new System.Drawing.Size(1073, 788);
             this.Load += new System.EventHandler(this.Calendar_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.CheckedListBox coursesCheckListBox;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
--- Connect-A-Bull/Calendar.cs
+++ Connect-A-Bull/Calendar.cs
@@ -10,37 +10,224 @@ using System.Windows.Forms;
 using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections;
 
 namespace Connect_A_Bull
 {
     public partial class Calendar : UserControl
     {
+        DateTime pickedDate = new DateTime();
+
         public Calendar()
         {
             InitializeComponent();
+
         }
 
         private void Calendar_Load(object sender, EventArgs e)
         {
-            var json_data = "";
+            EraseOldEvents();
+            GetNewEvents();
+            
+            treeView1.ExpandAll();
+            treeView1.Nodes[0].EnsureVisible();
+        }
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            pickedDate = dateTimePicker.Value;
+            EraseOldEvents();
+            GetNewEvents();
+        }
+
+        private void GetNewEvents()
+        {
+            if (pickedDate == DateTime.MinValue)
+            {
+                pickedDate = DateTime.Today;
+            }
+
+            DateTime monthDay = pickedDate.AddDays(30);
+            string canvasStartDay = pickedDate.ToString("yyyy-MM-dd");
+            string canvasEndDay = monthDay.ToString("yyyy-MM-dd");
+
+            string eventLinkPart1 = "https://canvas.instructure.com/api/v1/users/self/calendar_events?type=event&type=assignment&start_date=";
+            string eventLinkPart2 = "&end_date=";
+            string eventLinkPart3 = "&context_codes[]=course_";
+            string eventLinkPart4 = "&access_token=";
+
+            string json_data = "";
+            string canvasLinkCourses = "https://canvas.instructure.com/api/v1/users/self/courses?enrollment_state=active&access_token=";
+            string brittanyCanvas = "13~rIzSCEWS3pJVUHjbhaFhp2Y1c5owCk3WmQ6Iqr1o3MvTGS6FOFONQZjfsJgsPiNA";
+            string desCanvas = "13~UR54ZnsYuFhKMGipOJ9kn8yZX9gpd5IYOAp0zpYVYmZRC52GdZupc1qj4q59j8Qa";
+            string chosenUser = brittanyCanvas;
+            string userCourseLink = canvasLinkCourses + chosenUser;
+
             using (WebClient wc = new WebClient())
             {
-                 json_data= wc.DownloadString("https://canvas.instructure.com/api/v1/users/self/courses?enrollment_state=active&access_token=13~UR54ZnsYuFhKMGipOJ9kn8yZX9gpd5IYOAp0zpYVYmZRC52GdZupc1qj4q59j8Qa");
+                json_data = wc.DownloadString(userCourseLink);
             }
 
             JArray jsonArray = JArray.Parse(json_data);
             var jsonObjects = jsonArray.OfType<JObject>().ToList();
 
-           // String allCourses = "";
-            for(int i = 0; i< jsonObjects.Count;)
+            ArrayList courseIDs = new ArrayList();
+            ArrayList eventDates = new ArrayList();
+            DataTable fullEvents = new DataTable();
+            fullEvents.Columns.Add("Title", typeof(string));
+            fullEvents.Columns.Add("Date", typeof(string));
+            fullEvents.Columns.Add("Description", typeof(string));
+            int fullEventCount = 0;
+
+            string printTextBox = "";
+            string printTextBox2 = "";
+            
+
+   
+            //iterates over every course
+            for (int i = 0; i < jsonObjects.Count; i++)
             {
+                //1333005055 - used for retrieval of assignments
+                string courseID = jsonObjects[i]["id"].ToString();
+                courseIDs.Add(courseID);
 
+                //Software Engineering - CEN 4020
+                string tempCourse = "";
+                tempCourse += jsonObjects[i]["name"].ToString();
+                tempCourse += " - ";
+                string rawCourseCode = jsonObjects[i]["course_code"].ToString();
+                string rawAbreviation = rawCourseCode.Substring(0, 3);
+                string rawNumber = rawCourseCode.Substring(3, 4);
+                string courseCode = "No Course Code";
+                if (rawNumber[0] >= '0' && rawNumber[0] <= '9')
+                {
+                    courseCode = "";
+                    courseCode = rawAbreviation + " " + rawNumber;
+                }
+                tempCourse += courseCode;
+                //tempCourse += " " + courseID;
+                coursesCheckListBox.Items.Add(tempCourse);
+                printTextBox2 += tempCourse + "\r\n";
+
+                //Get events and assignments using current courseID
+                string eventFullLink = eventLinkPart1
+                     + canvasStartDay
+                     + eventLinkPart2
+                     + canvasEndDay
+                     + eventLinkPart3
+                     + courseIDs[i]
+                     + eventLinkPart4
+                     + chosenUser;
+
+                //Holds all events for given course
+                string json_data2 = "";
+                using (WebClient wc = new WebClient())
+                {
+                    json_data2 = wc.DownloadString(eventFullLink);
+                }
+
+                JArray jsonArray2 = JArray.Parse(json_data2);
+                var jsonObjects2 = jsonArray2.OfType<JObject>().ToList();
+
+                
+                //Iterates over all events for given course
+                for (int j = 0; j < jsonObjects2.Count; j++)
+                {
+                    
+                    //2d array, First Event is 1 array - [Event Title, Event Date/Due Date, Description]
+                    object[] tempEvent = new object[3];
+
+                    string titleEvent = jsonObjects2[j]["title"].ToString();
+                    tempEvent[0] = titleEvent;
+                    printTextBox += titleEvent + "\r\n";
+
+                    string dateEvent = jsonObjects2[j]["all_day_date"].ToString();
+                    tempEvent[1] = dateEvent;
+                    printTextBox += dateEvent + "\r\n";
+
+                    string descriptionEvent = jsonObjects2[j]["description"].ToString();
+                    tempEvent[2] = descriptionEvent;
+                    fullEventCount++;
+
+                    fullEvents.Rows.Add(tempEvent);
+                    //eventDates holds all of the event dates,
+                    //knows what Dates to display in tree view
+                    string tempEventDate = dateEvent; 
+                    eventDates.Add(tempEventDate);
+
+                    //printTextBox += "should match : " +  j + " " + fullEvents.Rows[courseWithEvents][0] + ", " + fullEvents.Rows[courseWithEvents][1] + "\r\n";
+                }//End iteration over events for given course
+            }//End iteration over every course
+
+
+            //debugging tool
+            //textBox1.Text = printTextBox;
+            printTextBox = "";
+            //textBox1.Text = printTextBox2;
+            //list to sort the and weed out duplicate dates
+            List<string> sortedEventDates = new List<string>();
+
+            //iterates over all the original dates
+            for (int j = 0; j < eventDates.Count; j++)
+            {
+                //adds the date to the new sorted/no duplicate list if doesn't exist in it already
+                if (!sortedEventDates.Contains(eventDates[j]))
+                {
+                    sortedEventDates.Add((string)eventDates[j]);
+                }
             }
-            label2.Text = jsonObjects[1]["name"].ToString();
 
+            //sorts the dates
+            sortedEventDates.Sort();
+            
+            for (int j = 0; j < sortedEventDates.Count; j++)
+            {
+                treeView1.Nodes.Add((string)sortedEventDates[j]);
+                treeView1.Nodes[j].NodeFont = new Font("Microsoft Sans Serif", 14);
+                treeView1.Nodes[j].BackColor = Color.FromArgb(0, 100, 132);
+                treeView1.Nodes[j].ForeColor = Color.White;
+            }
 
+            string forAddingNodes = "";
 
+            for (int j = 0; j < fullEventCount; j++)
+            {
+                int a = 0;
+                foreach (TreeNode n in treeView1.Nodes)
+                {
+                    string compareDate = (string)fullEvents.Rows[j][1];
+                    forAddingNodes += "Does \"" + compareDate + "\" equal \"" + n.Text + "\" ?\r\n";
+                    if(string.Compare(compareDate, n.Text) == 0)
+                    {
+                        treeView1.Nodes[a].Nodes.Add((string)fullEvents.Rows[j][0]);
+                    }
+                    a++;
+                }
+                
+            }
+            //textBox1.Text = forAddingNodes;
 
+            int everyDate = 0;
+            foreach(TreeNode n in treeView1.Nodes)
+            {
+                int b = 0;
+
+                foreach(TreeNode x in n.Nodes)
+                {
+                    treeView1.Nodes[everyDate].Nodes[b].NodeFont = new Font("Microsoft Sans Serif", 12);
+                    treeView1.Nodes[everyDate].Nodes[b].BackColor = Color.FromArgb(0, 120, 158); ;
+                    treeView1.Nodes[everyDate].Nodes[b].ForeColor = Color.White;
+                    b++;
+                }
+                everyDate++;
+            }
+
+        }
+
+        private void EraseOldEvents()
+        {
+            coursesCheckListBox.Items.Clear();
+            treeView1.Nodes.Clear();
         }
     }
 }
